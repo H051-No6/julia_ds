9b42e138c2c2da91c9222aac3307f6a5980b5549
Your Name <you@example.com>
2023-02-04 17:22:16 +0900

    fix_linear

--- src/LinearRegression.jl
+++ src/LinearRegression.jl
@@ -1,16 +1,39 @@
-function linear_regression(x::Matrix, y::Array)
-    xt = transpose(x)
-    xtx = xt*x
-    xty = xt*y
-    return inv(xtx)*xty
+using LinearAlgebra
+
+function fit(X, y, settings)
+    error("error! $settings is not supported type")
 end
 
-module LinearRegresser
-    lmd::Float64
-    function fit(x::Matrix, y::Array)
-        xt = transpose(x)
-        xtx = xt*x
-        xty = xt*y
-        return inv(xtx)*xty
+Base.@kwdef struct LinearRegression
+    add_const::Bool = false
+end
+
+function LinearRegression()
+    return LinearRegression(false)
+end
+
+function fit(X::Matrix, y::Array, model::LinearRegression)
+    if model.add_const
+        X = cat(X, ones(size(X)[1]), dims=2)
     end
+    xt = transpose(X)
+    xtx = xt*X
+    xty = xt*y
+    inv(xtx)*xty
+end
+
+Base.@kwdef struct RidgeRegression
+    lambda::Float64 = 0.
+    add_const::Bool = false
+end
+
+function RidgeRegression(lambda=0., add_const=false)
+    return RidgeRegression(lambda, add_const)
+end
+
+function fit(X::Matrix, y::Array, model::RidgeRegression)
+    xt = transpose(X)
+    xtx = xt*X
+    xty = xt*y
+    inv(xtx+model.lambda*I)*xty
 end
